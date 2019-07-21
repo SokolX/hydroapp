2404842c56f0260bda573322b5313800b16eb9a4
SokolX <89soek@gmail.com>
2019-07-21 21:15:57 +0200

    feat: change data to pwa

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 "use strict";
 
-import moment from 'moment';
+// service worker registration - remove if you're not going to use it
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 const key = new Date.toISOString().slice(0,10);
